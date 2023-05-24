b4faa07e67314543b5d1e2d8d51a3c40efb37819
Bode He <bode_he@epam.com>
2023-05-24 11:23:12 +0800

    feat: add auth page

---
+++ components/Input.tsx
@@ -0,0 +1,24 @@
+const Input = () => {
+    return (
+        <input 
+            className="
+                block
+                rounded-md
+                px-6
+                pt-6
+                pb-1
+                w-full
+                text-md
+                text-white
+                bg-neutral-700
+                appearance-none
+                focus:outline-none
+                focus:ring-o
+                peer
+            "
+            placeholder=" "
+        />
+    );
+}
+ 
+export default Input;
---
+++ pages/auth.tsx
@@ -0,0 +1,25 @@
+import Input from "@/components/Input";
+
+const Auth = () => {
+    return (
+        <div className="relative h-full w-ful bg-[url('/images/hero.jpg')] bg-no-repeat bg-fixed bg-center bg-cover">
+            <div className="bg-black w-full h-full lg:bg-opacity-50">
+                <nav className="px-12 py-5">
+                    <img src="/images/logo.png" alt="Logo" className="h-12" />
+                </nav>
+                <div className="flex justify-center">
+                    <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+                        <h2 className="text-white text-4xl mb-8 font-semibold">
+                            Sign in
+                        </h2>
+                        <div className="flex flex-col gap-4">
+                            <Input />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default Auth;
