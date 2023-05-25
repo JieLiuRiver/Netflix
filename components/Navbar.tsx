3a71ed7e16ec2b5007f3a31d2241ae7f23e1369b
Bode He <bode_he@epam.com>
2023-05-25 14:11:01 +0800

    feat: add scroll background effect for Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,15 @@
-import { useCallback, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { BsChevronDown, BsSearch, BsBell } from 'react-icons/bs'
 import NavbarItem from './NavbarItem';
 import MobileMenu from './MobileMenu';
 import AccountMenu from './AccountMenu';
 
+const TOP_OFFSET = 66;
+
 const NavBar = () => {
     const [showMobileMenu, setShowMobileMenu] = useState(false)
     const [showAccountMenu, setShowAccountMenu] = useState(false)
+    const [showBackground, setShowBackground] = useState(false)
 
     const toggleMobileMenu = useCallback(() => {
         setShowMobileMenu((value) => !value)
@@ -15,6 +18,23 @@ const NavBar = () => {
     const toggleAccountMenu = useCallback(() => {
         setShowAccountMenu((value) => !value)
     }, [])
+
+    useEffect(() => {
+        const handleScroll = () => {
+            console.log(window.scrollY)
+            if (window.scrollY >= TOP_OFFSET) {
+                setShowBackground(true)
+            } else {
+                setShowBackground(false)
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, []);
 
     const navbarDatas = useMemo(() => ([
         {
@@ -51,18 +71,7 @@ const NavBar = () => {
             className="w-full fixed z-40"
         >
             <div
-                className="
-                    px-4
-                    md:px-16
-                    py-6
-                    flex
-                    flex-row
-                    items-center
-                    transition
-                    duration-500
-                    bg-zinc-900
-                    bg-opaicy-90
-                "
+                className={`px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 ${showBackground ? 'bg-zinc-900 bg-opacity-90' : ''}`}
             >
                 <img className="h-4 lg:h-7" src="/images/logo.png" alt="Logo" />
                 <div
