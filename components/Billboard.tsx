b74e1c762a9f33d489138b6279d773c73ec40c94
Bode He <bode_he@epam.com>
2023-05-25 16:52:12 +0800

    feat: add play video feature

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -2,7 +2,7 @@ import React, { useCallback } from 'react';
 import { InformationCircleIcon } from '@heroicons/react/24/outline';
 
 import useBillboard from '@/hooks/useBillboard';
-// import PlayButton from '@/components/PlayButton';
+import PlayButton from '@/components/PlayButton';
 // import useInfoModalStore from '@/hooks/useInfoModalStore';
 
 const Billboard: React.FC = () => {
@@ -24,7 +24,7 @@ const Billboard: React.FC = () => {
           {data?.description}
         </p>
         <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
-          {/* <PlayButton movieId={data?.id} /> */}
+          <PlayButton movieId={data?.id} />
           <button
             // onClick={handleOpenModal}
             className="
